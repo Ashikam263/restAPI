d332f44720384dac813bdf2f377605a1da8f3e8a
Ashikam263 <ashikam263@gmail.com>
2024-05-03 17:59:31 +0530

    credentilas changed

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,8 @@ AppDataSource.initialize()
 const pool = new Pool({
   user: 'postgres', 
   host: 'localhost',
-  database: 'your_database_name', 
-  password: 'your_password', 
+  database: 'mydatabase', 
+  password: 'password', 
   port: 5432, 
 });
 
